e67dcf7294f436d55f8b8229eb5462527cf8daa6
Administrator <himanshu00qwerty@gmail.com>
2020-12-20 18:07:19 +0530

    k

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Level4 from './Components/Level4/Level4';
 import Level5 from './Components/Level5/Level5';
 import Level3 from './Components/Level3/Level3';
@@ -10,62 +10,58 @@ import Stage2 from './Components/Stage2/Stage2';
 import Stage3 from './Components/Stage3/Stage3';
 function App() {
   return (
-    <Router>
-      <Switch>
+    <Switch>
+      {/* for testing the components */}
+      <Route path="/test">
+        <Test hint={"guess the ghrase"} />
+      </Route>
 
-        {/* for testing the components */}
-        <Route path="/test">
-          <Test hint={"guess the ghrase"} />
-        </Route>
-
-        {/* stage1 */}
-        <Route path="/level5">
-          <div className="cloud__class">
-            <div className="bg top">
-              <Level5 />
-            </div>
+      {/* stage1 */}
+      <Route path="/level5">
+        <div className="cloud__class">
+          <div className="bg top">
+            <Level5 />
           </div>
-        </Route>
-        {/* stage1 homepage */}
-        <Route path="/stage1">
-          <Stage1 />
-        </Route>
+        </div>
+      </Route>
+      {/* stage1 homepage */}
+      <Route path="/stage1">
+        <Stage1 />
+      </Route>
 
-        {/* stage2 */}
-        <Route path="/level4">
-          <div className="cloud__class">
-            <div className="bg top">
-              <Level4 />
-            </div>
+      {/* stage2 */}
+      <Route path="/level4">
+        <div className="cloud__class">
+          <div className="bg top">
+            <Level4 />
           </div>
-        </Route>
-        {/* stage1 homepage */}
-        <Route path="/stage2">
-          <Stage2 />
-        </Route>
+        </div>
+      </Route>
+      {/* stage1 homepage */}
+      <Route path="/stage2">
+        <Stage2 />
+      </Route>
 
-        {/* stage3 */}
-        <Route path="/level3">
-          <div className="cloud__class">
-            <div className="bg top">
-              <Level3 />
-            </div>
+      {/* stage3 */}
+      <Route path="/level3">
+        <div className="cloud__class">
+          <div className="bg top">
+            <Level3 />
           </div>
-        </Route>
-        {/* stage3 homepage */}
-        <Route path="/stage3">
-          <Stage3 />
-        </Route>
+        </div>
+      </Route>
+      {/* stage3 homepage */}
+      <Route path="/stage3">
+        <Stage3 />
+      </Route>
 
-        {/* default */}
-        <Route path="/">
-          <div className="cloud__class__1">
-            <MainScreen />
-          </div>
-        </Route>
-
-      </Switch>
-    </Router>
+      {/* default */}
+      <Route path="/">
+        <div className="cloud__class__1">
+          <MainScreen />
+        </div>
+      </Route>
+    </Switch>
   );
 }
 
